a52600d02c102310b71033aa2375d596cfc56c5f
Liskowskyy <polax22pl@gmail.com>
2024-09-05 22:53:57 +0200

    Add endpoint for target langs listing

---
+++ api/list-target-langs.php
@@ -0,0 +1,35 @@
+<?php
+    //Set content type
+    header('Content-Type: application/json; charset=utf-8');
+
+    //Set Composer root
+    require_once(__DIR__."/../vendor/autoload.php");
+
+    //Load config
+    require("config.php");
+
+    $apiKey = $DeepLAPIKey; //Get key from config.php
+
+    $deepl = new \DeepL\Translator($apiKey);
+
+    //Enclose in try to return an error if API Key is invalid
+    try {
+        $targetLanguages = $deepl->getTargetLanguages(); //Get targets langs from API
+
+        //Return list
+        $response["status"] = "success";
+        $response["data"] = $targetLanguages;
+
+        echo json_encode($response);
+        
+    }
+    catch(Exception $e) {
+        //Get error message and return it
+        $response["status"] = "error";
+        $response["message"] = $e->getMessage();
+
+        echo json_encode($response);
+    }
+
+
+?>
