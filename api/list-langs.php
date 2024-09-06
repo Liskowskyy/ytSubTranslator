daaa8b684d23b863e9048dd1ddb856634bf7ccd2
Liskowskyy <polax22pl@gmail.com>
2024-09-06 05:09:05 +0200

    List auto detect as a source lang

--- api/list-langs.php
+++ api/list-langs.php
@@ -15,6 +15,8 @@
     //Enclose in try to return an error if API Key is invalid
     try {
         $sourceLanguages = $deepl->getSourceLanguages(); //Get source langs from API
+        $autoDetectLang = ["name" => "Detect language", "code" => null, "supportsFormality" => null]; //Add element for auto detection
+        array_unshift($sourceLanguages, $autoDetectLang); //Add auto detection to target lang list
         $targetLanguages = $deepl->getTargetLanguages(); //Get targets langs from API
 
         //Return list
