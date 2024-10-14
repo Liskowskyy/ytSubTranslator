65138cc4c5dfd6e560ae1a06c1beb5c81f2b1950
Liskowskyy <polax22pl@gmail.com>
2024-10-15 01:26:40 +0200

    GUI: Save translations instead of blobs as base64 to local storage

--- index.php
+++ index.php
@@ -230,22 +230,12 @@
     <script type="text/javascript" src="jszip.min.js"></script>
     <script type="text/javascript" src="FileSaver.min.js"></script>
     <script>
-        function saveToLocalStorage(blob, filename, origFile) {
+        function saveToLocalStorage(translations, filename, origFile) {
             let uuid = self.crypto.randomUUID();
-            const reader = new FileReader();
+            let timestamp = Date.now();
 
-            reader.onload = (event) => {
-                let base64 = event.target.result;
-                let dataType = base64.split(';base64,')[0];
-                let content = base64.split(';base64,')[1];
-                let timestamp = Date.now();
-
-                let toStore = JSON.stringify({dataType: dataType, content: content, timestamp: timestamp, filename: filename, origFile: origFile});
-
-                localStorage.setItem(uuid, toStore);
-            }
-
-            reader.readAsDataURL(blob);
+            let toStore = JSON.stringify({translations: translations, timestamp: timestamp, filename: filename, origFile: origFile});
+            localStorage.setItem(uuid, toStore);
         }
 
         //Form POST handler
@@ -287,7 +277,7 @@
                         let filename = new Date().toLocaleString()+"  "+langCode;
                         filename = filename.replace(/[^a-z0-9]/gi, '-')+".srt";
                         saveAs(blob, filename);
-                        saveToLocalStorage(blob, filename, origFile);
+                        saveToLocalStorage(translations, filename, origFile);
                     }
                     else if(translationsCodes.length > 1) {
                         //Save into a .zip archive if multiple translations
@@ -302,7 +292,7 @@
                                 let filename = new Date().toLocaleString();
                                 filename = filename.replace(/[^a-z0-9]/gi, '-')+".zip";
                                 saveAs(blob, filename);
-                                saveToLocalStorage(blob, filename, origFile);
+                                saveToLocalStorage(translations, filename, origFile);
                         });
                     }
                 },
