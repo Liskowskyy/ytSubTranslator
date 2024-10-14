20ffbeeddb33bc915202fd0644a8d5b00821dd81
Liskowskyy <polax22pl@gmail.com>
2024-10-14 14:10:30 +0200

    GUI: Prepare for translation downloader

--- saved.php
+++ saved.php
@@ -29,6 +29,18 @@
 <body style="margin-left: 5%; margin-right: 5%;">
     <h1 class="text-center col-lg-6 offset-lg-3">Saved translations</h1>
 
+    <div id="savedList" class="col-lg-4 offset-lg-4">
+        <div class="card">
+        <div class="card-header">
+            Translation
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Time and date</h5>
+            <p class="card-text">Small excerpt from translation</p>
+            <a href="#" class="btn btn-primary">Download</a>
+        </div>
+        </div>
+    </div>
 
     <script
     src="https://code.jquery.com/jquery-3.7.1.min.js"
@@ -43,6 +55,13 @@
     <script type="text/javascript" src="FileSaver.min.js"></script>
     <script>
         //TODO: Downloader for translations saved into application memory
+        function loadItems() {
+            const items = {...localStorage};
+            console.log(items);
+        }
+
+        $( document ).ready(loadItems); //Load items on page load
+        window.addEventListener("storage", loadItems, false); //Reload items on any change
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
